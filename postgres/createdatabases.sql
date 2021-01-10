d8004ca2a1ecc2d7655ab14bbc5bf4af700b9961
Dušan Jovanović <dusandjovanovic@outlook.com>
2021-01-10 19:21:58 +0100

    Minor db script change

--- postgres/createdatabases.sql
+++ postgres/createdatabases.sql
@@ -1,14 +1,18 @@
 CREATE USER nsi_user WITH ENCRYPTED PASSWORD 'nsi_pass';
 
+-- payments
 CREATE DATABASE nsi_payments;
 GRANT ALL PRIVILEGES ON DATABASE nsi_payments TO nsi_user;
 
+--hangfire
 CREATE DATABASE nsi_jobs;
 GRANT ALL PRIVILEGES ON DATABASE nsi_jobs TO nsi_user;
 
+--policy
 CREATE DATABASE nsi_policy;
 GRANT ALL PRIVILEGES ON DATABASE nsi_policy TO nsi_user;
 
+--pricing
 CREATE DATABASE nsi_pricing;
 GRANT ALL PRIVILEGES ON DATABASE nsi_pricing TO nsi_user;
 
